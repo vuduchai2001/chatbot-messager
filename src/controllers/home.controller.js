c7d6dfbddb414fe4ed625a35f09c9ad8e1fe85bb
ha1vd <hai13032001@gmail.com>
2024-05-29 21:38:13 +0700

    add send message

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 
-let postWebhook = (req, res) => {
+const postWebhook = (req, res) => {
   let body = req.body;
 
   if (body.object === "page") {
     body.entry.forEach((e) => {
       let webhook_event = e.messaging[0];
       console.log(webhook_event);
+
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      }
+      // else if (webhook_event.postback) {
+      //   handlePostback(sender_psid, webhook_event.postback);
+      // }
     });
     res.status(200).send("EVENT_SUCCESS");
   } else {
@@ -14,7 +24,7 @@ let postWebhook = (req, res) => {
   }
 };
 
-let getWebhook = (req, res) => {
+const getWebhook = (req, res) => {
   let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   // Parse the query params
@@ -33,23 +43,48 @@ let getWebhook = (req, res) => {
   }
 };
 
-// function verifyRequestSignature(req, res, buf) {
-//   var signature = req.headers["x-hub-signature-256"];
+const handleMessage = async (sender_psid, received_message) => {
+  let messageText = received_message.text?.toLowerCase().replace(/\s+/g, "");
+  let response = { text: "Hải dương 34 chào anh em" };
 
-//   if (!signature) {
-//     console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
-//   } else {
-//     var elements = signature.split("=");
-//     var signatureHash = elements[1];
-//     var expectedHash = crypto
-//       .createHmac("sha256", config.appSecret)
-//       .update(buf)
-//       .digest("hex");
-//     if (signatureHash != expectedHash) {
-//       throw new Error("Couldn't validate the request signature.");
-//     }
-//   }
-// }
+  console.log(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------"
+  );
+  console.log("yourInput--", messageText);
+  console.log(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------"
+  );
+
+  callSendAPI(sender_psid, response);
+  res.status(200).send("MESSAGE SENDED");
+};
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  console.log("RESPONSE-", request_body);
+  let infoRes = {
+    uri: "https://graph.facebook.com/v2.6/me/messages",
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: "POST",
+    json: request_body,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(infoRes, (err, res, body) => {
+    if (!err) {
+      console.log("message sent!");
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
 
 module.exports = {
   postWebhook: postWebhook,
